0713de2410f306fac17587fdf832a2d47df53fe7
qshh <3218696007@qq.com>
2025-09-21 21:20:24 +0800

    fix(android):  添加国内maven镜像修复依赖下载慢

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,19 @@
 allprojects {
     repositories {
-        google()
-        mavenCentral()
+        // 阿里云镜像
+        maven { setUrl("https://maven.aliyun.com/repository/public/") }
+        maven { setUrl("https://maven.aliyun.com/repository/central/") }
+        maven { setUrl("https://maven.aliyun.com/repository/google/") }
+        maven { setUrl("https://maven.aliyun.com/repository/jcenter/") }
+        maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin/") }
+        // 华为云镜像
+        maven { setUrl("https://repo.huaweicloud.com/repository/maven/") }
+        // 腾讯云镜像
+        maven { setUrl("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
+        // 网易镜像
+        maven { setUrl("https://mirrors.163.com/maven/repository/maven-public/") }
+//        google()
+//        mavenCentral()
     }
 }
 
